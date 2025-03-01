99af1e2fbcd0f515c63c82271315f9980327dafd
Sealight Clock <chbebram@hotmail.com>
2025-03-01 13:11:53 -0800

    3rd commit: Updated .gitignore for .DS_Store and build.    Trying to make running of QtQuick app from Qt Creator.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QDir>
 
 int main(int argc, char *argv[]) {
     QGuiApplication app(argc, argv);
     QQmlApplicationEngine engine;
+
+    // Explicitly set the QML module import path for Qt Creator
+    engine.addImportPath(QCoreApplication::applicationDirPath() + "/../Resources");
 
     // Load the QML module named "transitions", starting from Main.qml
     engine.loadFromModule("transitions", "Main");
